05d0542b0da8b8ae6da5e2cbb1111100a69681ac
oabdulaji <oabdulaji@tessa.group>
2025-10-08 15:46:48 +0200

    Functions 1

---
+++ Functions/bifunctions.py
@@ -0,0 +1,46 @@
+# Fuctions nelerdir ?
+# Birden fazla parametre alan fonksiyonlara bifunction denir.
+# Def ile tanimlanir.
+
+# def selamlama():
+#     print("Merhaba")
+#     print("Nasılsın....?")
+
+# selamlama()  # Fonksiyon cagirma islemi
+
+# def birinci():
+#     print("Ben ilk fonksiyonum")
+
+# def ikinci():
+#     print("Ben ikinci fonksiyonum")
+
+# sayi = int(input("Bir sayi giriniz: "))
+# if sayi > 0 :
+#     birinci()
+# else:
+#     ikinci()
+
+# def selamlama():
+#     print("Merhaba")
+#     print("Nasılsın....?")
+
+# selamlama("Volkan")   
+
+# def selamlama(isim):
+#     print("Merhaba", isim)
+
+# selamlama("Volkan")
+
+# def toplama(a, b , c):
+#     toplam = a + b + c
+#     print("Toplam:", toplam)
+
+# toplama(3,4,5)
+
+# def ortalama(liste):
+#     total = 0
+#     for i in liste:
+#         total += i
+#     print(total / len(liste))
+
+# ortalama([3,4,5,6,7,100,-50,55,999,156])
