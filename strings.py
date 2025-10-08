dc43a667d151ef1e37a2a89fbdf29fe27195b03c
oabdulaji <oabdulaji@tessa.group>
2025-10-08 10:37:37 +0200

    Stringler ile islemler

---
+++ strings.py
@@ -0,0 +1,45 @@
+# String Method
+
+# lower
+# upper
+# split
+# Toplama ve Cikarma
+
+# "Merhaba Python"
+
+# 'Merhaba Python'
+
+# "I'm Onur"
+
+# 'Herkese "Merhaba"'
+
+# Lower
+
+# a = "HELLO WORLD"
+
+# a.lower()
+
+# Upper
+
+# b = "hello world"
+
+# b.upper()
+
+# Split
+
+# b = "hello world"
+
+# b.split()
+
+# c = "Kirmizi , Sari , Yesil , Mavi"
+
+# c.split(",")
+
+# a = "Merhaba"
+# b = "Python"
+
+# a+b
+
+# kelime = "Python"
+
+# kelime*3
