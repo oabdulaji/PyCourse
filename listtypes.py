061f05c451efb90038a962e40fdcfdcce3fae902
oabdulaji <oabdulaji@tessa.group>
2025-10-08 10:58:08 +0200

    Listeler

---
+++ listtypes.py
@@ -0,0 +1,23 @@
+# Liste Veri Tipi
+
+# my_list = [1,2,3.4,"Python"]
+
+# my_list[1]
+# my_list[3]
+
+
+# liste = [0,1,2,3,4,5,6,7,8,9]
+
+# liste[0:5]
+
+# liste[0:6]
+
+# liste[0:6:2]
+
+# liste[2:5]
+
+# liste[2::2]
+
+# liste[::-1]
+
+# liste[5:1:-1]
