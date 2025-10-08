30e4dbde654c17c09d52e6110b33ed5ee45c6cd0
oabdulaji <oabdulaji@tessa.group>
2025-10-08 11:08:39 +0200

    Liste Metotlari

---
+++ listemetotlari.py
@@ -0,0 +1,42 @@
+# Liste Metotlari
+
+#Append , pop , sort
+
+
+list2 = [121 , 1 , -4 , 99 , 7 , 10]
+
+list2.append("Python")
+
+list2
+
+list2.pop()  # sondaki elemani siler ve dondurur
+
+list2
+
+list2.pop(2)  # index numarasina gore siler ve dondurur
+
+list2 = [121 , 1 , -4 , 99 , 7 , 10]
+
+list2
+
+list2.sort()  # kucukten buyuge siralar
+
+list2
+
+list2.sort(reverse=True)  # buyukten kucuge siralar
+
+list2
+
+list3 = [7,8.8,9,"Merhaba"]
+
+list2 + list3  # iki listeyi birlestirir
+
+list3 * 3  # listeyi 3 kere tekrarlar
+
+# Nested Listeler (ici ice listeler)
+
+list3 = [[1,2] , ["a" , "b"] , [7 , 8]]
+
+list3[0][1]  # ilk listeyi getirir
+
+list3[2][0]  # 7 sayisini getirir
