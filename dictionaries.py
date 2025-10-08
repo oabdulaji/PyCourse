5c9589f8f4b5262ffd623f85e2e1e236d09981f7
oabdulaji <oabdulaji@tessa.group>
2025-10-08 11:18:13 +0200

    Sozlukler

---
+++ dictionaries.py
@@ -0,0 +1,28 @@
+# Sozluk Veri Tipleri (Dictionaries)
+
+scores = {"Onur":85,"Ecren":75,"Ali":45,"Mehmet":78}
+
+scores
+
+scores["Onur"] 
+
+scores["Ecren"]=99
+
+scores
+
+scores["Hakan"]=65
+
+scores
+
+
+scores.keys() # Anahtarlar
+
+scores.values() # Degerler
+
+scores.items() # Anahtar ve Degerler
+
+scores2 = {"Erkan":88,"Fatma":44}
+
+scores.update(scores2) # Birlesme
+
+scores
