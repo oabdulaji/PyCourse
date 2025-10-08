6bfa97b9df320107faa35c36121f77f52ef371af
oabdulaji <oabdulaji@tessa.group>
2025-10-08 22:17:13 +0200

    Open And Close

---
+++ Documents/OpenAndClose.py
@@ -0,0 +1,12 @@
+
+
+dosya = open("dosya.txt", "w")
+
+dosya.write("Merhaba Python")
+
+dosya.close()
+
+dosya = open("dosya-1.txt","w",encoding="utf8")
+dosya.write("Bu Bir Testir")
+dosya.close()
+
