e0d78dac2b103950c898cabec806ec068c3b19ab
oabdulaji <oabdulaji@tessa.group>
2025-10-08 16:06:30 +0200

    Lambda Funcs

---
+++ Lambda.py
@@ -0,0 +1,26 @@
+# Lambda Fonksiyonlar nedir ?
+# Lambda fonksiyonlar, Python'da tek satırlık anonim fonksiyonlardır.
+# Genellikle basit işlemler için kullanılırlar ve "lambda" anahtar kelimes
+# ile tanımlanırlar.
+# Lambda fonksiyonların sözdizimi şu şekildedir:
+# lambda parametreler: ifade
+
+# def double(x):
+#     return x * 2
+
+# double(4)  # Çıktı: 8
+
+# double1 = lambda x: x * 2
+
+# double1(4)  # Çıktı: 8
+
+# type(double1(4))  # Çıktı: <class 'function'>
+
+
+# def toplam(a,b):
+#     return a + b
+
+# toplam(3,5)
+
+# toplam1 = lambda a,b: a + b
+# toplam1(3,5)
