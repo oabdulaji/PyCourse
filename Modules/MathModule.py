a0c4b8eebeb7b280ae567a74e767dc7f2b7fc810
oabdulaji <oabdulaji@tessa.group>
2025-10-08 16:28:38 +0200

    Math Module

---
+++ Modules/MathModule.py
@@ -0,0 +1,45 @@
+# Python Math Module Nedir ?
+# Python Math Module Nasıl İmport Edilir ?
+# Python Math Module İçerisindeki Fonksiyonlar Nelerdir ?
+# Python Math Module Kullanımı
+# Python Math Module Örnekleri
+
+# import math # Math modülünü import etme
+
+# dir(math) # Math modülündeki fonksiyonları ve sabitleri listeleme
+
+# help(math) # Math modülündeki fonksiyonlar hakkında bilgi alma
+
+# def faktoriyel(sayi):
+#     faktoriyel = 1
+#     if (sayi == 0 or sayi == 1):
+#         print("Faktoriyel: 1")
+#     else:
+#         while sayi > 1:
+#             faktoriyel *= sayi
+#             sayi -= 1
+#             print("Faktoriyel Degerimiz : ", faktoriyel)
+
+# faktoriyel(10)
+
+# math.factorial(5) # Math modülündeki faktoriyel fonksiyonu kullanımı
+# math.factorial(10) # Math modülündeki faktoriyel fonksiyonu kullanımı
+
+# 2 ci yontem
+
+# from math import * # Math modülündeki tüm fonksiyonları ve sabitleri import etme
+
+# factorial(5) # Math modülündeki faktoriyel fonksiyonu kullanımı
+# factorial(10) # Math modülündeki faktoriyel fonksiyonu kullanımı
+
+# sin(1) # Math modülündeki sinüs fonksiyonu kullanımı
+# cos(1) # Math modülündeki kosinüs fonksiyonu kullanımı
+# floor(5.9) # Math modülündeki floor fonksiyonu kullanımı
+# ceil(5.1) # Math modülündeki ceil fonksiyonu kullanımı
+# pow(4,2) # Math modülündeki üs alma fonksiyonu kullanımı
+
+# from math import factorial, sin, cos, floor, ceil, pow # Math modülündeki belirli fonksiyonları ve sabitleri import etme
+
+# import math as matematik
+
+# matematik.factorial(5)
