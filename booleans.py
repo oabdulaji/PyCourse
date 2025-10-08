d5542627e4ab42d3de712d5400d5a64eb666f08e
oabdulaji <oabdulaji@tessa.group>
2025-10-08 11:25:01 +0200

    Bool Operatorler

---
+++ booleans.py
@@ -0,0 +1,56 @@
+# Booelans Opeatorler
+
+a = True
+
+a
+
+b = False
+
+b
+
+# And Or Not Operatorler
+
+a and b
+
+b = True
+
+a and b
+
+
+a = False
+
+a or b
+
+b = False
+
+a or b
+
+not b
+
+not a
+
+c = True
+
+not c
+
+# Karsilastirma Operatorler
+
+2 == 2 
+
+2 == 3
+
+2 != 3
+
+2 != 2
+
+5 > 2
+
+2 > 5
+
+5 >= 5
+
+5 >= 6
+
+7 <= 9
+
+7 <= 4
