827c19350bf37247f2bd3275713e8b8cdea12ffb
oabdulaji <oabdulaji@tessa.group>
2025-10-08 12:24:33 +0200

    Range Fonksiyonu

---
+++ rangeFonksiyonlari.py
@@ -0,0 +1,22 @@
+# Range Fonksiyonu
+
+liste = [1,2,3,4,5,6,7,8,9]
+
+list1 = list(range(10))  # 0 dan 9 a kadar sayilar
+print(list1)
+
+list2 = list(range(-10,100)) # -10 dan 99 a kadar sayilar
+print(list2)
+
+list3 = list(range(0,20,2)) # 0 dan 20 ye kadar cift sayilar
+print(list3)
+
+print(*range(0,10))
+
+print(list(range(10)))
+
+num = range(5,15)
+print(*num)
+
+
+print(list(range(10,0,-1))) # 10 dan 1 e kadar sayilar
