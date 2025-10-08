ba10e99d0a38a75b5ffd1737df681f818dd5e19e
oabdulaji <oabdulaji@tessa.group>
2025-10-08 16:53:09 +0200

    Time Module

---
+++ Modules/TimeModule.py
@@ -0,0 +1,23 @@
+# Time Module Python Nedir ? 
+# Time Module Python Ne İşe Yarar ?
+# Time Module Python Nasıl Kullanılır ?
+# Time Module Python Örnek Kullanımlar
+# Time Module Python Fonksiyonları Nelerdir ?
+
+
+import time
+
+saniye = time.time()
+print(saniye)
+
+time.ctime()
+
+
+time.strftime("%Y")
+
+time.strftime("%d/%m/%y")   
+
+
+time.strftime("%c")
+
+time.strftime("%H:%M:%S %p")
