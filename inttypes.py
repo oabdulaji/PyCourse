a35afd65aec6245f6d23ecd8e16c179463f0d1a9
oabdulaji <oabdulaji@tessa.group>
2025-10-08 08:52:34 +0200

    Python Integer Types And Workflow

---
+++ inttypes.py
@@ -0,0 +1,41 @@
+#Sayilar
+
+# int Tam Sayi 
+# float Kusuratli Sayi
+
+# + Toplama
+# - Cikarma
+# * Carpma
+# /  Bolme 
+# // Tam Sayi Bulma
+# % Kalan
+
+# 15 + 7
+
+# 15 + 22.5
+
+# 15 - 24 
+
+# 7.9 - 4.0
+
+# 7 - 1.2
+
+# 4 * 2
+
+# 1.9 * 7.8
+
+# 10 / 2
+
+# 7.9 / 9.8
+
+# 9 / 2 
+
+# 9 // 2
+
+# 10 % 5 
+
+# 11 % 5
+
+# 9 % 4 
+
+# 9 % 5 
