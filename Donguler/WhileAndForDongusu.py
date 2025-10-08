5d46e9ba60d3051e192c0e54bcc3e259bd5762cf
oabdulaji <oabdulaji@tessa.group>
2025-10-08 14:31:50 +0200

    For ve While Donguleri

---
+++ Donguler/WhileAndForDongusu.py
@@ -0,0 +1,46 @@
+# While Dongusu
+
+# Kod bloklarini belirli bir kosul saglandigi surece tekrar tekrar calistirmak icin kullanilir.
+
+
+# sayac = 5
+# while sayac >= 0:
+#     print(sayac)
+#     sayac -= 1  # sayac = sayac - 1
+    
+
+# sayac = 10
+# while sayac >= 0 :
+#     sayac -= 2
+#     print(sayac)
+
+# number = 0
+# while number < 10:
+#     print(number)
+#     number += 2
+
+# sayac = 5
+# while sayac > 0 :
+#     print(sayac)
+#     sayac -= 1
+# else:
+#     print("Sayac Degeri Bitti / Else Blogu Calisti")
+
+
+# For Dongusu
+
+# isimler = ["Onur", "Abdulaji", "Tessa", "Group"]
+# for isim  in isimler:
+#     print(isim)
+
+# kordinatlar = (3,4,5)
+# for x in kordinatlar:
+#     print(x)
+
+# for i in range(0,50,2):  # 0,1,2,3,4
+#     print(i)
+
+# for i in range(0,10):
+#     print(i)
+# else:
+#     print("Dongu Bitti")
