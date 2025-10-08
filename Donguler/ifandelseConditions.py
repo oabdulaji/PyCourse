837be037ce9aae540644d40ab48c6957dc8540ba
oabdulaji <oabdulaji@tessa.group>
2025-10-08 13:45:11 +0200

    If Else Durumlari

---
+++ Donguler/ifandelseConditions.py
@@ -0,0 +1,38 @@
+# If ve Else Koşulları
+
+# skor = int(input("Lütfen notunuzu giriniz: "))
+# if skor > 50:
+#     print("Dersten Geçtiniz")
+# else:
+#     print("Dersten Kaldınız")
+
+
+# yas = int(input("Lütfen yasinizi giriniz: "))
+# if yas > 18:
+#     print("Yasiniz : " , yas , "Bize Katilabilirsiniz")
+# else:
+#     print("Yasiniz 18 den kucuk oldugu icin bize katilamazsiniz")
+# print("Hicbir kosula tabi degilim") # Bu burada apayri bir satir olarak calisir
+
+
+
+# ulke = input("Lütfen ülkenizi giriniz....: ")
+# agirlik = int(input("Lütfen agirligini giriniz....: "))
+# if ulke == "USA":
+#     if agirlik > 10:
+#         print("10 Kg dan daha fazla kargolar icin ek ucret alinir.")
+#     else:
+#         print("Ortalama gonderim zamani 5 gundur.")
+# else:
+#     print("Ortalama gonderim zamani 15 gundur.")
+
+# ulke = input("Lütfen ülkenizi giriniz....: ")
+# agirlik = int(input("Lütfen agirligini giriniz....: "))
+# if ulke == "USA" and agirlik > 100:
+#     print("Kargonuz cok agir malesef gonderemiyoruz.")
+
+
+# ulke = input("Lütfen ülkenizi giriniz....: ")
+# agirlik = int(input("Lütfen agirligini giriniz....: "))
+# if ulke == "TR" or ulke == "GR" :
+#     print("Turkiye ve Almanya icin ek gonderim zamani gereklidir.") 
