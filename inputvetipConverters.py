da047f9c262ac8903289228d8a990fb1225fa13f
oabdulaji <oabdulaji@tessa.group>
2025-10-08 12:18:03 +0200

    Input Ve Donusumler

---
+++ inputvetipConverters.py
@@ -0,0 +1,33 @@
+# Input Fonksiyonu ve Tip Donusumleri
+
+# kullanici_adi = input("Kullanici adinizi giriniz: ")
+# print(kullanici_adi)
+
+# yas = input("Yasinizi giriniz: ")
+# print('Yasiniz : ', yas)
+
+# isim = input("Isminizi giriniz: ")
+# soyisim = input("Soyisminizi giriniz: ")
+# yas = input("Yasinizi giriniz: ")
+
+# print('Isminiz : {} , Soyadiniz : {} , Yasiniz : {}'.format(isim, soyisim, yas))
+
+# yas = int(input("Yasinizi giriniz: "))
+
+# print(type(yas))
+
+# kenar = int(input("Karenin bir kenarini giriniz: "))
+# print('Karenin Alani : {}'.format(kenar * kenar))
+
+# string_numara = '11'
+# int_sayi = int(string_numara)
+
+# print(type(int_sayi))
+# print(type(string_numara))
+
+# sayi = 11 
+# string_sayi = str(sayi)
+# print(type(sayi))
+# print(type(string_sayi))
+
+# kenar = int(input("Karenin bir kenarini giriniz: ")) # Error 
