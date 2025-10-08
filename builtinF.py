6c0fc47743e45bb4b6229e5a228c5539bec377c8
oabdulaji <oabdulaji@tessa.group>
2025-10-08 11:29:54 +0200

    Built In Functions

---
+++ builtinF.py
@@ -0,0 +1,26 @@
+# Built In Functions
+
+# Type Fonskiyonu - Veri Tipini Öğrenme
+
+
+type(2.88) # float
+
+selamlama = "Merhaba"
+type(selamlama) # str
+
+type(True) # bool  
+
+liste = [1,2,3]
+type(liste) # list
+
+# Len Fonksiyonu - Uzunluk Öğrenme
+
+len(liste) # 3
+
+len(selamlama) # 7
+
+# Print Fonksiyonu - Ekrana Yazdırma
+
+print("Merhaba Dünya") # Merhaba Dünya
+
+print(selamlama) # Merhaba
